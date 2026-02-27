7d2f255229b6a6379c5b2348698d42d952e07eae
Miguel Castro <miguel.castro@sga.pucminas.br>
2026-02-27 13:48:51 -0300

    Adição de edição, deleção e inserção de Chirps. Sem validação

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index']);
+Route::resource('chirps', ChirpController::class)
+    ->only(['store', 'edit', 'update', 'destroy']);
